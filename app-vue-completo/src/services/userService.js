dd107e6501fb0419bb2affda34a62623488ddefe
genilson.martins <genilson.martins@virtus.ufcg.edu.br>
2024-09-02 11:42:53 -0300

    projetos

--- app-vue-completo/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const gitHubUrl = 'https://api.github.com/users'
-
-export default {
-    obterTodos(){
-        return axios.get(gitHubUrl)
-    }
-}
--- node-demos/ex1/app.js
+++
@@ -1,5 +0,0 @@
-const calculadora = require('./utils/calculadora')
-
-console.log('Olá node')
-const soma = calculadora.somar(5, 3)
-console.log(soma)
