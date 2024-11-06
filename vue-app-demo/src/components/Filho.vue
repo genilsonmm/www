7ed83dae30c277a8679d052f7fd0a94007fbfff6
genilson.martins <genilson.martins@virtus.ufcg.edu.br>
2024-11-06 18:53:11 -0300

    app-vue

---
+++ vue-app-demo/src/components/Filho.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Filho</h1>
+        {{ msg }}
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps(['msg'])
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #000;
+    }
+</style>
